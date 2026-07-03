eb16d3b4014a02bc98da47137782123ab0372791
Nica <nicaanje@hotmail.com>
2026-07-03 12:42:48 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from rich.console import Console
+from rich_gradient import print_gradient
+
+console = Console()
+
+print_gradient("Hello, Gradient!", colors=["#ff0000", "#ffff00", "#00ff00", "#00ffff", "#0000ff"])
